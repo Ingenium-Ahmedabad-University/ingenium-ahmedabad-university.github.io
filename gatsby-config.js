7915abfa228c96d2173d6e7c3876522c17b8e431
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-02-07 23:01:30 +0530

    Added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
+const resolveConfig = require('tailwindcss/resolveConfig');
+const tailwindConfig = require('./tailwind.config.js');
+
+const { theme } = resolveConfig(tailwindConfig);
+
 module.exports = {
   siteMetadata: {
     title: 'Ingenium',
+    author: 'Ingenium tech team',
+    description: 'A Gatsby starter to set you up with Tailwind CSS',
   },
   plugins: [
     'gatsby-plugin-sharp',
@@ -14,6 +21,35 @@ module.exports = {
         path: './src/images/',
       },
       __key: 'images',
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Ingenium',
+        short_name: 'Ingenium',
+        start_url: '/',
+        background_color: theme.colors.white,
+        theme_color: theme.colors.extended,
+        icon: 'static/icon.svg',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('tailwindcss'), require('autoprefixer')],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `raleway`,
+          `oswald`,
+          `abril fatface`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+      },
     },
   ],
 };
