052f96f9810d9278a1be3af116f3635e2bb5592f
arpitvaghela <arpitvaghela9210@gmail.com>
2021-02-12 03:16:29 +0530

    added bideo fullscreen on scroll (need fix), added polaroid frame for images

--- src/components/e_selection.jsx
+++ src/components/e_selection.jsx
@@ -1,39 +1,71 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'gatsby';
 
 const e_selction_data = [
   {
     title: 'Workshop',
     src:
-      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+      'https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
   }
 ];
-const EventSelection = () => {
+const EventSelectionBlock = () => {
   const data = e_selction_data;
+  const [show_video,set_show_video] = useState(false); 
+  const inputref = useRef(null);
+    const scrollHandler = _ => {
+    let rect = inputref.current.getBoundingClientRect();
+    set_show_video(
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    )
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler, true);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler, true);
+    };
+  }, []);
   return (
-    <>
+    <div ref={inputref}>
       {data.map((e, i) => 
-      <Link >
-      <div className="h-screen w-full max-h-screen overflow-hidden md:w-10/12  md:max-h-3/4-screen md:mx-auto video-wrapper">
+      <Link   id="v-link">
+      <VideoBlock show={show_video} src={e.src}/>
+      </Link>
+      )}
+      </div>
+  );
+};
+const VideoBlock = ({show,src}) => {
+  return (
+      <div className={"video-wrapper "+(show?"":"hidden")}>
         <div className="backdrop grid place-content-center">
             <div className="text-6xl text-white">Workshops</div>
           </div>
+
           <video
             id='player1'
             className='h-full w-full'
-            src={e.src}
-            // onMouseEnter={event => event.target.play()}
-            // onMouseOut={event => event.target.pause()}
+            src={src}
             loop
             muted
             autoPlay
             preload='auto'
           />
       </div>
-      </Link>
-      )}
-      </>
-  );
-};
+  )
 
-export default EventSelection;
+}
+// const isInViewport= (el)=> {
+//   // const rect = el.getBoundingClientRect();
+//   return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+//     );
+// }
+export default EventSelectionBlock;
