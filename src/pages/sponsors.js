a8edf7319476218ecfc1d1410cbe86cb6f4e67bb
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-03-03 22:09:50 +0530

    Working build

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -4,16 +4,17 @@ import Header from '../components/header';
 import Sponsors from '../components/sponsors';
 import Footer from '../components/footer';
 
-const SponsorsPage = () => 
-        <div>
-            <SEO />
-            <div className='base px-3 md:px-5 lg:px-10 bg-base'>
-                <Header />
-                <Sponsors />
-                <Footer />
-            </div>
-        </div>
-    
+const SponsorsPage = () => (
+  <div>
+    <SEO />
+    <div className='base px-3 md:px-5 lg:px-10 bg-base'>
+      <Header />
+      <Sponsors />
+      <Footer />
+    </div>
+  </div>
+);
+
 export default () => {
-    return <SponsorsPage />;
+  return <SponsorsPage />;
 };
