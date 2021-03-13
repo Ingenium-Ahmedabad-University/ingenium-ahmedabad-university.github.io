82f1084317b712b895e06d21e6633aa7b07a7771
Henil-747 <henil280102@gmail.com>
2021-03-13 21:38:07 +0530

    added data and updated contact_us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -4,7 +4,6 @@ import Header from '../components/header';
 import Contact from '../components/contact-us';
 import Footer from '../components/footer';
 
-<<<<<<< HEAD
 const ContactUS = () => 
         <div>
             <SEO />
@@ -18,19 +17,3 @@ const ContactUS = () =>
 export default () => {
     return <ContactUS />;
 };
-=======
-const ContactUS = () => (
-  <div>
-    <SEO />
-    <div className='base px-3 md:px-5 lg:px-10 bg-base'>
-      <Contact />
-      <Footer />
-      <Header />
-    </div>
-  </div>
-);
-
-export default () => {
-  return <ContactUS />;
-};
->>>>>>> 552f176bd6d01dbb55d2f82db7157b6353b0ca14
