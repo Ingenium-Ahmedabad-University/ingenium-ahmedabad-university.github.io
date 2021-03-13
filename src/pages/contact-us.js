01130bfb6139727543fe8c46e2a8379ced77c0c3
Vinay Kakkad <kakkadvinay25@gmail.com>
2021-03-13 16:39:40 +0000

    fixing build fail

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -4,7 +4,7 @@ import Header from '../components/header';
 import Contact from '../components/contact-us';
 import Footer from '../components/footer';
 
-const ContactUS = () => 
+const ContactUs = () => (
         <div>
             <SEO />
             <div className='base px-3 md:px-5 lg:px-10 bg-base'>
