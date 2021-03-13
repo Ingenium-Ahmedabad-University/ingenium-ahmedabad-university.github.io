736d72cd090347cd2191619b495a6ed0f9e26bcd
Arpit Vaghela <50636734+arpitvaghela@users.noreply.github.com>
2021-03-13 22:10:31 +0530

    Henil v2 (#23)
    * updated contact page
    
    * formatted code
    
    * updated contact page
    
    * added data and updated contact_us page
    
    * fixing build fail
    
    Co-authored-by: Henil-747 <henil280102@gmail.com>
    Co-authored-by: Vinay Kakkad <kakkadvinay25@gmail.com>

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -4,33 +4,15 @@ import Header from '../components/header';
 import Contact from '../components/contact-us';
 import Footer from '../components/footer';
 
-<<<<<<< HEAD
-const ContactUS = () => 
+const ContactUs = () => (
         <div>
             <SEO />
             <div className='base px-3 md:px-5 lg:px-10 bg-base'>
-                <Header />
                 <Contact />
                 <Footer />
+                <Header />
             </div>
         </div>
-    
-export default () => {
-    return <ContactUS />;
-};
-=======
-const ContactUS = () => (
-  <div>
-    <SEO />
-    <div className='base px-3 md:px-5 lg:px-10 bg-base'>
-      <Contact />
-      <Footer />
-      <Header />
-    </div>
-  </div>
 );
 
-export default () => {
-  return <ContactUS />;
-};
->>>>>>> 552f176bd6d01dbb55d2f82db7157b6353b0ca14
+export default ContactUs;
