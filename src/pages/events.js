d17fe57da795d3143f1c216aed087e0e09049578
foram-s1 <foram.s1@ahduni.edu.in>
2021-03-15 00:40:36 +0530

    Navigation issue fixed

--- src/pages/events.js
+++ src/pages/events.js
@@ -8,7 +8,6 @@ const Eventpage = () => (
   <>
     <SEO />
     <div className='px-3 md:px-5 lg:px-10 top-0 absolute bg-base w-screen overflow-x-hidden'>
-      <Header />
       <EventLists />
       <Footer />
       <Header />
