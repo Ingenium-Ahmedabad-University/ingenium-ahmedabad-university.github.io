8b512c0821ab5108739c754c78a9ee3546cda140
foram-s1 <foram.s1@ahduni.edu.in>
2021-03-23 16:41:05 +0530

    Data added

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -11,6 +11,7 @@ import e9 from '../images/auction.jpeg';
 import '../css/custom.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Link } from 'gatsby';
 
 const events_data = [
   {
@@ -50,6 +51,7 @@ const events_data = [
             title: 'Careers in CSE',
             date: '27th March, 12 pm',
             image: e7,
+            url: '',
           },
           {
             desc:
@@ -57,6 +59,7 @@ const events_data = [
             date: '21st March, 4 pm',
             title: 'Kickstart IOT',
             image: e2,
+            url: 'kickstart-iot',
           },
         ],
       },
@@ -64,14 +67,14 @@ const events_data = [
         type: 'Chemical',
         detail: [
           {
-            desc:
-              'Enhance your knowledge!',
+            desc: 'Enhance your knowledge!',
             title: 'Talk on Project Management ',
             date: '27th March, 12 pm',
             image: e8,
+            url: '',
           },
-        ]
-      }
+        ],
+      },
     ],
   },
   {
@@ -85,24 +88,28 @@ const events_data = [
             date: '3rd April, 2-5 pm',
             title: 'Codefi',
             image: e5,
+            url: '',
           },
           {
             desc: 'Upcoming!!',
             date: '3rd April, 5-7 pm',
             title: 'E-treasure Hunt',
             image: e3,
+            url: 'e-treasure-hunt',
           },
           {
             desc: 'Upcoming!!',
             date: '2nd April, 8am',
             title: 'Kaggle Competition',
             image: e4,
+            url: 'kaggle-competition',
           },
           {
             desc: 'Upcoming!!',
             date: '4th April, 10-6 pm',
             title: 'Iot Auction',
             image: e9,
+            url: 'iot-auction',
           },
         ],
       },
@@ -110,24 +117,30 @@ const events_data = [
         type: 'Chemical',
         detail: [
           {
-            desc: 'Test your problem-solving and tactical skills to overcome hinderance of the production of the end product',
+            desc:
+              'Test your problem-solving and tactical skills to overcome hinderance of the production of the end product',
             date: '2nd-3rd April, 1pm onwards',
             title: 'Need for solution',
             image: e1,
+            url: '',
           },
           {
-            desc: 'A quiz-based event that contains the structure of testing wits, strategies, and knowledge of a true Chemist/Chemical Engineer!',
+            desc:
+              'A quiz-based event that contains the structure of testing wits, strategies, and knowledge of a true Chemist/Chemical Engineer!',
             date: '3rd April, 10-12am',
             title: 'Chemical Hunt ',
             image: e1,
+            url: '',
           },
           {
-            desc: 'An ultimate competition where teams compete with each other to reach the treasure!',
+            desc:
+              'An ultimate competition where teams compete with each other to reach the treasure!',
             date: '4th April, 10-12 pm',
             title: 'Road to treasure',
             image: e1,
+            url: '',
           },
-        ]
+        ],
       },
       {
         type: 'Mechanical',
@@ -138,33 +151,38 @@ const events_data = [
             title: 'Mechathon V2.0',
             date: ' 27-28 March, 9am-5pm',
             image: e6,
+            url: '',
           },
           {
             desc: 'Upcoming!!',
             date: 'TBA',
             title: 'Quiz on Automobiles',
             image: e1,
+            url: '',
           },
           {
             desc: 'Upcoming!!',
             date: 'TBA',
             title: 'Meme Story',
             image: e1,
+            url: '',
           },
           {
             desc: 'Upcoming!!',
             date: 'TBA',
             title: 'E-Gaming Fests',
             image: e1,
+            url: '',
           },
           {
             desc: 'Upcoming!!',
             date: 'TBA',
             title: 'Identify the car',
             image: e1,
+            url: '',
           },
         ],
-      }
+      },
     ],
   },
 ];
@@ -178,7 +196,10 @@ const EventLists = () => {
     <div className='sm:mx-12 mx-6 my-12 lg:mx-18 pb-5'>
       {data.map((el, key) => (
         <div>
-          <h1 id={el.category} className='event_title text-3xl md:text-5xl text-center pt-16 pb-3 bg-gradient-to-br from-red-500 to-indigo-400 social-bg'>
+          <h1
+            id={el.category}
+            className='event_title text-3xl md:text-5xl text-center pt-16 pb-3 bg-gradient-to-br from-red-500 to-indigo-400 social-bg'
+          >
             {el.category}
           </h1>
           <div className='underline mx-auto md:w-64 w-40 h-1 items-center bg-gradient-to-br from-red-500 to-indigo-400'>
@@ -196,24 +217,25 @@ const EventLists = () => {
               </div>
               <div className='grid grid-cols-1 gap-12 lg:grid-cols-3 sm:grid-cols-2 xl:gap-20 md:gap-16 py-10'>
                 {typ.detail.map((det, key1) => (
-                  <div>
+                  <div> <Link to={`${det.url}`}>
                     <div
                       className='avatar relative h-64 w-full flex justify-center items-center text-center bg-cover bg-center'
                       data-aos='fade-up'
                       style={{ backgroundImage: `url(${det.image})` }}
                     >
-                      <div className='absolute top-0 mt-20 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900'></div>
+                      
+                        <div className='absolute top-0 mt-20 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900'></div>
 
-                      <div className='desc z-10 tracking-tight bg-gray-800 bg-opacity-60 text-white'>
-                        <span className=' font-bold md:text-2xl lg:text-3xl'>
-                          {det.title}
-                        </span>
-                        <span className='detail px-2 pb-2'>
-                          <span className='text-lg'>{det.date}</span>
-                          <br /> {det.desc}
-                        </span>
-                      </div>
-                    </div>
+                        <div className='desc z-10 tracking-tight bg-gray-800 bg-opacity-60 text-white'>
+                          <span className=' font-bold md:text-2xl lg:text-3xl'>
+                            {det.title}
+                          </span>
+                          <span className='detail px-2 pb-2'>
+                            <span className='text-lg'>{det.date}</span>
+                            <br /> {det.desc}
+                          </span>
+                        </div>
+                    </div> </Link>
                     <div className='md_640px bg-black bg-opacity-50 rounded-b-xl p-2'>
                       <span className='font-bold text-lg'>{det.title}</span>
                       <br />
