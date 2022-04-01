659a1b3671189001d3967ff467fb2acf0bab4c22
Henil-747 <henil280102@gmail.com>
2022-04-01 22:51:44 +0530

    updated contact us

--- src/components/contact-us.jsx
+++ src/components/contact-us.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import picture1 from '../images/Nisarg_Thoriya.jpg';
-import picture2 from '../images/Kaushal_Patil.jpg';
+import picture1 from '../images/Nisarg_Thoriya_updated.jpg';
+import picture2 from '../images/Kaushal_Patil_updated.jpg';
 import picture3 from '../images/Ankit_Devani.jpg';
 import picture4 from '../images/Viraj_Boghani.jpg';
 import picture5 from '../images/Kashish_Jivani.jpg';
@@ -16,6 +16,13 @@ import picture14 from '../images/Anshi_Shah.jpg';
 import picture15 from '../images/Yash_Longani.jpg';
 import picture16 from '../images/Krina_Khakhariya.jpeg';
 import picture17 from '../images/Vinay_Kakkad.jpg';
+import picture18 from "../images/Prof_Akhand_Rai.jpeg";
+import picture19 from "../images/Prof_Jayendra_Bhalodiya.jpeg";
+import picture20 from "../images/Tirth_Patel.jpeg";
+import picture21 from "../images/Poojan_Gandhi.jpeg";
+import picture22 from "../images/Nipun_Patel.jpeg";
+import picture23 from "../images/Jinesh_Salot_updated.jpeg";
+import picture24 from "../images/Henil_Shah.jpg";
 
 const icon1 =
   'https://cdn.jsdelivr.net/npm/simple-icons@4.12.0/icons/whatsapp.svg';
@@ -23,27 +30,36 @@ const icon2 =
   'https://cdn.jsdelivr.net/npm/simple-icons@4.10.0/icons/linkedin.svg';
 const icon3 =
   'https://cdn.jsdelivr.net/npm/simple-icons@4.12.0/icons/gmail.svg';
-const team_details = [
+
+const advisory_team_details = [
   {
     id: 1,
-    name: 'Nisarg Thoriya',
-    image: picture1,
-    role: 'Advisor ',
-    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+917779033222',
-    gmail_Url_Url: 'mailto:shivang.g@ahduni.edu.in',
-    linkedLn_Url: 'https://www.linkedin.com/in/shivanggohil',
+    name: 'Akhand Rai',
+    image: picture18,
+    role: 'Faculty Advisor',
   },
   {
     id: 2,
-    name: 'Kaushal Patil ',
-    image: picture2,
-    role: 'Advisor',
-    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+917043132142',
-    gmail_Url_Url: 'mailto:kaushal.p@ahduni.edu.in',
-    linkedLn_Url: 'https://www.linkedin.com/in/kaushal1011/',
+    name: 'Jayendra Bhalodiya',
+    image: picture19,
+    role: 'Faculty Advisor',
   },
   {
     id: 3,
+    name: 'Nisarg Thoriya',
+    image: picture1,
+    role: 'Advisor',
+  },
+  {
+    id: 4,
+    name: 'Kaushal Patil ',
+    image: picture2,
+    role: 'Advisor',
+  }
+];
+const core_team_details = [
+  {
+    id: 1,
     name: 'Ankit Devani',
     image: picture3,
     role: 'Committee Head',
@@ -52,7 +68,7 @@ const team_details = [
     linkedLn_Url: 'https://www.linkedin.com/in/nisarg-thoriya/',
   },
   {
-    id: 4,
+    id: 2,
     name: 'Viraj Boghani',
     image: picture4,
     role: 'Committee Head',
@@ -62,7 +78,7 @@ const team_details = [
   },
 
   {
-    id: 5,
+    id: 3,
     name: 'Kashish Jivani',
     role: 'Secretary',
     image: picture5,
@@ -72,17 +88,17 @@ const team_details = [
   },
 
   {
-    id: 6,
-    name: 'Neel Popat',
+    id: 4,
+    name: 'Harsh Patel ',
     role: 'Treasurer',
-    image: picture8,
+    image: picture6,
     whatsapp_Url: 'https://api.whatsapp.com/send?phone=+917600303434',
     gmail_Url_Url: 'mailto:arpitsinh.v@ahduni.edu.in',
     linkedLn_Url: 'https://www.linkedin.com/in/arpitvaghela/',
   },
 
   {
-    id: 7,
+    id: 5,
     name: 'Krunal Savaj',
     role: 'Registration & Marketing Head',
     image: picture7,
@@ -92,7 +108,17 @@ const team_details = [
   },
 
   {
-    id: 9,
+    id: 6,
+    name: 'Neel Popat',
+    role: 'Sponsorship & Logistic head',
+    image: picture8,
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+918320949532',
+    gmail_Url_Url: 'mailto:neel.p@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/viraj-boghani-a270671b1',
+  },
+
+  {
+    id: 7,
     name: 'Jay Mehta',
     role: 'CSE Event Head',
     image: picture9,
@@ -102,7 +128,7 @@ const team_details = [
   },
 
   {
-    id: 10,
+    id: 8,
     name: 'Jainil Siddhapura',
     role: 'Mechanical Event Head',
     image: picture10,
@@ -112,7 +138,7 @@ const team_details = [
   },
 
   {
-    id: 11,
+    id: 9,
     name: 'Saurav Pajvani',
     role: 'Chemical Event Head',
     image: picture11,
@@ -122,7 +148,7 @@ const team_details = [
   },
 
   {
-    id: 12,
+    id: 10,
     name: 'Aditi Vasa',
     role: 'Content Team Head',
     image: picture12,
@@ -132,7 +158,7 @@ const team_details = [
   },
 
   {
-    id: 13,
+    id: 11,
     name: 'Kenil Gondaliya',
     role: 'Design Team Head',
     image: picture13,
@@ -142,7 +168,7 @@ const team_details = [
   },
 
   {
-    id: 14,
+    id: 12,
     name: 'Anshi Shah',
     role: 'Social Media Head',
     image: picture14,
@@ -152,7 +178,7 @@ const team_details = [
   },
 
   {
-    id: 15,
+    id: 13,
     name: 'Yash Longani',
     role: 'Hackathon Lead',
     image: picture15,
@@ -161,7 +187,7 @@ const team_details = [
     linkedLn_Url: 'https://www.linkedin.com/in/sanket-shah-978828167/',
   },
   {
-    id: 16,
+    id: 14,
     name: 'Krina Khakhariya',
     role: 'Decoration Head',
     image: picture16,
@@ -169,19 +195,90 @@ const team_details = [
     gmail_Url_Url: 'mailto:sanket.s1@ahduni.edu.in',
     linkedLn_Url: 'https://www.linkedin.com/in/sanket-shah-978828167/',
   },
+];
 
+const tech_team_details = [
   {
-    id: 17,
+    id: 1,
     name: 'Vinay Kakkad',
     role: 'Tech Lead',
     image: picture17,
-    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+916354973976',
-    gmail_Url_Url: 'mailto:sanket.s1@ahduni.edu.in',
-    linkedLn_Url: 'https://www.linkedin.com/in/sanket-shah-978828167/',
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+919429479019',
+    gmail_Url_Url: 'mailto:vinay.k@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/vinaykakkad',
+  },
+
+  {
+    id: 2,
+    name: 'Tirth Patel',
+    role: 'Developer',
+    image: picture20,
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+918200612084',
+    gmail_Url_Url: 'mailto:tirth.p5@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/tirth-patel-412b70192',
+  },
+
+  {
+    id: 3,
+    name: 'Nipun Patel',
+    role: 'Developer',
+    image: picture21,
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+917990224584',
+    gmail_Url_Url: 'mailto:nipun.p@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/nipun-patel-9a18101ab',
+  },
+
+  {
+    id: 4,
+    name: 'Poojan Gandhi',
+    role: 'Developer',
+    image: picture22,
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+919510923972',
+    gmail_Url_Url: 'mailto:poojan.g@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/poojan-gandhi-8b77931a5',
+  },
+  {
+    id: 5,
+    name: 'Jinesh Salot',
+    role: 'Developer',
+    image: picture23,
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+919106810725',
+    gmail_Url_Url: 'mailto:jinesh.s1@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/jinesh-salot-502a071a6',
+  },
+  {
+    id: 6,
+    name: 'Henil Shah',
+    role: 'Developer',
+    image: picture24,
+    whatsapp_Url: 'https://api.whatsapp.com/send?phone=+919512120133',
+    gmail_Url_Url: 'mailto:shah.h@ahduni.edu.in',
+    linkedLn_Url: 'https://www.linkedin.com/in/henil-shah-095837190',
   },
 ];
 
-const ImgTemplate = ({
+const AdvisorImgTemplate = ({
+  name,
+  image,
+  role,
+}) => {
+  /* const [hoverRef, isHovered] = useState(false); */
+  return (
+    <div>
+      <div
+        class='relative mx-auto rounded-full object-cover bg-cover max_img_size_advisor img_padding'
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <br />
+      <div class='text-center text-white font-bold text-xl'>{name}</div>
+      <br />
+      <div class='text-center text-white font-bold italic text-xl'>{role}</div>
+      <br />
+    </div>
+  );
+};
+
+const CoreImgTemplate = ({
   name,
   image,
   role,
@@ -198,11 +295,9 @@ const ImgTemplate = ({
       ></div>
       <br />
       <div class='text-center text-white font-bold text-xl'>{name}</div>
-      <br />
-      <div class='text-center text-white font-bold italic text-xl'>{role}</div>
-      <br />
+      <div class='text-center text-white py-4 font-bold italic text-xl'>{role}</div>
 
-      <div class='grid grid-cols-3 justify-center align-middle place-items-center object-cover icon_padding'>
+      <div class='grid grid-cols-3 justify-center py-3 align-middle place-items-center object-cover icon_padding'>
         <a href={whatsapp_Url}>
           <img
             src={icon1}
@@ -235,6 +330,58 @@ const ImgTemplate = ({
   );
 };
 
+const TechImgTemplate = ({
+  name,
+  image,
+  role,
+  whatsapp_Url,
+  gmail_Url_Url,
+  linkedLn_Url,
+}) => {
+  /* const [hoverRef, isHovered] = useState(false); */
+  return (
+    <div>
+      <div
+        class='relative mx-auto rounded-full bg-center object-cover bg-cover max_img_size img_padding'
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <br />
+      <div class='text-center text-white font-bold text-xl'>{name}</div>
+      <div class='text-center py-3 text-white font-bold italic text-xl'>{role}</div>
+
+      <div class='grid grid-cols-3 py-3 justify-center align-middle place-items-center object-cover icon_padding'>
+        <a href={whatsapp_Url}>
+          <img
+            src={icon1}
+            height='22'
+            width='22'
+            alt=''
+            class='hover:invert-5 invert-7'
+          />
+        </a>
+        <a href={linkedLn_Url}>
+          <img
+            src={icon2}
+            height='22'
+            width='22'
+            alt=''
+            class='hover:invert-5 invert-7'
+          />
+        </a>
+        <a href={gmail_Url_Url}>
+          <img
+            src={icon3}
+            height='22'
+            width='22'
+            alt=''
+            class='hover:invert-5 invert-7'
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
 const Contact_Us = () => {
   return (
     <code class='font-sans'>
@@ -245,10 +392,14 @@ const Contact_Us = () => {
           <div class='font-bold bg-gradient-to-br from-secondary to-secondary-dark social-bg text-center py-14 text-4xl'>
             MEET THE TEAM
           </div>
-          <div class='grid grid-cols-1 justify-center items-center py-8 gap-x-12 gap-y-36 sm:grid-cols-2 lg:grid-cols-3'>
-            {team_details.map((e) => {
+          <div class = "core-team">
+          <div class='font-bold bg-gradient-to-br from-secondary to-secondary-dark social-bg py-14 text-3xl'>
+            ADVISORS
+          </div>  
+          <div class='grid grid-cols-1 justify-center items-center py-8 gap-x-12 gap-y-32 sm:grid-cols-2'>
+            {advisory_team_details.map((e) => {
               return (
-                <ImgTemplate
+                <AdvisorImgTemplate
                   name={e.name}
                   image={e.image}
                   role={e.role}
@@ -258,6 +409,47 @@ const Contact_Us = () => {
                 />
               );
             })}
+          </div>
+          </div>
+
+          <div class = "core-team">
+          <div class='font-bold bg-gradient-to-br from-secondary to-secondary-dark social-bg py-14 text-3xl'>
+            CORE TEAM
+          </div>  
+          <div class='grid grid-cols-1 justify-center items-center py-8 gap-x-12 gap-y-32 sm:grid-cols-2 lg:grid-cols-3'>
+            {core_team_details.map((e) => {
+              return (
+                <CoreImgTemplate
+                  name={e.name}
+                  image={e.image}
+                  role={e.role}
+                  whatsapp_Url={e.whatsapp_Url}
+                  gmail_Url_Url={e.gmail_Url_Url}
+                  linkedLn_Url={e.linkedLn_Url}
+                />
+              );
+            })}
+          </div>
+          </div>
+
+          <div class = "tech-team">
+          <div class='font-bold bg-gradient-to-br from-secondary to-secondary-dark social-bg py-14 text-3xl'>
+            TECH TEAM
+          </div>
+          <div class='grid grid-cols-1 justify-center items-center py-8 gap-x-16 gap-y-32 sm:grid-cols-3 lg:grid-cols-4'>
+            {tech_team_details.map((e) => {
+              return (
+                <TechImgTemplate
+                  name={e.name}
+                  image={e.image}
+                  role={e.role}
+                  whatsapp_Url={e.whatsapp_Url}
+                  gmail_Url_Url={e.gmail_Url_Url}
+                  linkedLn_Url={e.linkedLn_Url}
+                />
+              );
+            })}
+          </div>
           </div>
         </div>
       </div>
